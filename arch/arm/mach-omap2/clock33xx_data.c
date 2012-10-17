1bfbe3bf1a6532b4e76896ca14ce6f69f03729a5
Vaibhav Hiremath <hvaibhav@ti.com>
2012-10-17 13:55:55 -0500

    ARM: AM33XX: Fix configuration of dmtimer parent clock by dmtimer driver
    Add dmtimer clock aliases for AM33XX devices so that the parent clock for
    the dmtimer can be set correctly by the dmtimer driver. Without these clock
    aliases the dmtimer driver will fail to find the parent clocks for the dmtimer.
    
    Verified that DMTIMERs can be successfully requested on AM335x beagle bone.
    
    Original patch was provided by Vaibhav Hiremath [1]. Changelog and
    additional verification performed by Jon Hunter.
    
    [1] http://marc.info/?l=linux-omap&m=134693631608018&w=2
    
    Signed-off-by: Vaibhav Hiremath <hvaibhav@ti.com>
    Signed-off-by: Jon Hunter <jon-hunter@ti.com>
    Tested-by: Jon Hunter <jon-hunter@ti.com>

--- arch/arm/mach-omap2/clock33xx_data.c
+++ arch/arm/mach-omap2/clock33xx_data.c
@@ -1084,6 +1084,8 @@ static struct omap_clk am33xx_clks[] = {
 	CLK(NULL,	"sysclkout_pre_ck",	&sysclkout_pre_ck,	CK_AM33XX),
 	CLK(NULL,	"clkout2_ck",		&clkout2_ck,	CK_AM33XX),
 	CLK("da8xx_lcdc.0",     NULL,           &lcdc_fck,      CK_AM33XX),
+	CLK(NULL,	"timer_32k_ck",		&clkdiv32k_ick,	CK_AM33XX),
+	CLK(NULL,	"timer_sys_ck",		&sys_clkin_ck,	CK_AM33XX),
 };
 
 int __init am33xx_clk_init(void)
