4924e6547c2ffc682101141a4617c4a4dade15e7
Pantelis Antoniou <panto@antoniou-consulting.com>
2012-10-25 20:45:19 +0300

    omap4-opp: Add 446x to the list of OPP supporting omap4s

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -93,7 +93,7 @@ int __init omap4_opp_init(void)
 {
 	int r = -ENODEV;
 
-	if (!cpu_is_omap443x())
+	if (!cpu_is_omap443x() && !cpu_is_omap446x())
 		return r;
 
 	r = omap_init_opp_table(omap44xx_opp_def_list,
