1441e84b15e94f7a1d681d25f12fbea61b52075f
Pantelis Antoniou <panto@antoniou-consulting.com>
2012-10-29 14:43:56 +0200

    capebus: Remove unused enabled attribute

--- drivers/capebus/capebus-sysfs.c
+++ drivers/capebus/capebus-sysfs.c
@@ -33,41 +33,6 @@
 
 #include <linux/capebus.h>
 
-static ssize_t is_enabled_store(struct device *dev,
-				struct device_attribute *attr, const char *buf,
-				size_t count)
-{
-	struct cape_dev *cdev = to_cape_dev(dev);
-	unsigned long val;
-	ssize_t result = strict_strtoul(buf, 0, &val);
-
-	if (result < 0)
-		return result;
-
-	/* this can crash the machine when done on the "wrong" device */
-	if (!capable(CAP_SYS_ADMIN))
-		return -EPERM;
-
-	if (!val) {
-		if (capebus_is_enabled(cdev))
-			capebus_disable_device(cdev);
-		else
-			result = -EIO;
-	} else
-		result = capebus_enable_device(cdev);
-
-	return result < 0 ? result : count;
-}
-
-static ssize_t is_enabled_show(struct device *dev,
-			       struct device_attribute *attr, char *buf)
-{
-	struct cape_dev *cdev;
-
-	cdev = to_cape_dev(dev);
-	return sprintf(buf, "%u\n", atomic_read(&cdev->enable_cnt));
-}
-
 static ssize_t id_show(struct device *dev,
 			       struct device_attribute *attr, char *buf)
 {
@@ -78,7 +43,6 @@ static ssize_t id_show(struct device *dev,
 }
 
 struct device_attribute capebus_dev_attrs[] = {
-	__ATTR(enable, 0600, is_enabled_show, is_enabled_store),
 	__ATTR(id, 0400, id_show, NULL),
 	__ATTR_NULL,
 };
