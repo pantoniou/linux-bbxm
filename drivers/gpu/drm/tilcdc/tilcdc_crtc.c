edab05ed7ff169b39fa82fca1e4334cff60a9c82
Pantelis Antoniou <panto@antoniou-consulting.com>
2013-06-06 22:55:04 +0300

    tilcdc: More refined audio mode compatibility check.
    Allow audio modes more selectively. This should end up in DT instead
    but it will do for release.
    
    Conflicts:
    	drivers/gpu/drm/tilcdc/tilcdc_crtc.c

--- drivers/gpu/drm/tilcdc/tilcdc_crtc.c
+++ drivers/gpu/drm/tilcdc/tilcdc_crtc.c
@@ -454,14 +454,33 @@ int tilcdc_crtc_max_width(struct drm_crtc *crtc)
 	return max_width;
 }
 
+/* this should find it's way to DT */
+static int audio_mode_check(int hdisplay, int vdisplay, int refresh,
+		int cea_mode)
+{
+	/* only cea modes can handle audio */
+	if (cea_mode <= 0)
+		return 0;
+
+	/* hardcoded mode that supports audio (at 24Hz) */
+	if (hdisplay == 1920 && vdisplay == 1080 && refresh == 24)
+		return 1;
+
+	/* from the rest, only those modes that refresh at 50/60 */
+	if (refresh != 50 && refresh != 60)
+		return 0;
+
+	return 1;
+}
+
 int tilcdc_crtc_mode_valid(struct drm_crtc *crtc, struct drm_display_mode *mode,
 		int rb_check, int audio, struct edid *edid)
 {
 	struct tilcdc_drm_private *priv = crtc->dev->dev_private;
 	unsigned int bandwidth;
 	uint32_t hbp, hfp, hsw, vbp, vfp, vsw;
-	int has_audio, is_cea_mode;
-
+	int has_audio, is_cea_mode, can_output_audio, refresh;
+	uint8_t cea_mode;
 	int rb;
 
 	/*
@@ -486,10 +505,16 @@ int tilcdc_crtc_mode_valid(struct drm_crtc *crtc, struct drm_display_mode *mode,
 	has_audio = edid && drm_detect_monitor_audio(edid);
 
 	/* set if it's a cea mode */
-	is_cea_mode = drm_match_cea_mode(mode) > 0;
+	cea_mode = drm_match_cea_mode(mode);
+	is_cea_mode = cea_mode > 0;
 
-	DBG("mode %dx%d@%d pixel-clock %d audio %s cea %s",
-		mode->hdisplay, mode->vdisplay, drm_mode_vrefresh(mode),
+	/* set if we can output audio */
+	can_output_audio = edid && has_audio &&
+			audio_mode_check(mode->hdisplay, mode->vdisplay,
+					refresh, cea_mode);
+
+	DBG("mode %dx%d@%d pixel-clock %d audio %s cea %s can_output %s",
+		mode->hdisplay, mode->vdisplay, refresh,
 		mode->clock,
 		has_audio ? "true" : "false",
 		is_cea_mode ? "true" : "false");
